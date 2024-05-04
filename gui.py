915d411e31695d7885d9734ddef47d5255c32b42
Sean Beaulieu <54913639+seanbeaulieu@users.noreply.github.com>
2024-05-04 12:09:57 -0500

    basic word functionality at fixed interval

--- gui.py
+++ gui.py
@@ -26,59 +26,66 @@ class Platinum(tk.Tk):
         self.load_words()
         self.load_noti_words()
 
-        # Set up notification thread
-        self.notification_thread_stop_event = threading.Event()
-        self.notification_thread = threading.Thread(target=self.display_notifications, args=(5,), daemon=True)
-        self.notification_thread.start()
-
-        # Toggle notification button
-        self.toggle_button = tk.Button(self, text="Pause Notifications", command=self.toggle_notifications)
+        # toggle notification button
+        self.toggle_button = tk.Button(self, text="Start Notifications", command=self.toggle_notifications)
         self.toggle_button.pack()
 
-        # Add/Delete buttons
+        # add/delete buttons
         self.add_to_noti_button = tk.Button(self, text="Add to Notifications", command=self.add_to_noti_words)
         self.add_to_noti_button.pack()
         self.delete_button = tk.Button(self, text="Delete Word", command=self.delete_word)
         self.delete_button.pack()
 
-        # System tray
+        # system tray
         self.tray_icon = None
         self.protocol("WM_DELETE_WINDOW", self.hide_window)
 
+    # save a word to the words.json file
     def add_word(self):
         word = self.word_entry.get()
         self.word_manager.add_word(word)
         self.word_entry.delete(0, tk.END)
         self.load_words()
 
+    # load all the words into gui word_list
     def load_words(self):
         self.word_list.delete(0, tk.END)
         words = self.word_manager.get_words()
         for word in words:
             self.word_list.insert(tk.END, word)
 
+    # load all the words from noti_words.json
     def load_noti_words(self):
         self.noti_words_list.delete(0, tk.END)
         for word in self.word_manager.noti_words:
             self.noti_words_list.insert(tk.END, word)
 
+    # function for thread to call to display a toast every interval
+    # loops through all notification words (currently not random)
     def display_notifications(self, interval):
         while not self.notification_thread_stop_event.is_set():
             if self.word_manager.noti_words:
-                word = self.word_manager.noti_words[0]
-                self.toaster.show_toast("platinum", word, icon_path="icon.ico", duration=1)
-                self.word_manager.noti_words.pop(0)
-                self.load_noti_words()
-            time.sleep(interval)
+                for word in self.word_manager.noti_words:
+                    if self.notification_thread_stop_event.is_set():
+                        break
+                    self.toaster.show_toast("platinum", word, icon_path="icon.ico", duration=1)
+                    time.sleep(interval)
+            else:
+                time.sleep(interval)
 
+    # function to toggle the notification thread on or off (restarts)
     def toggle_notifications(self):
-        if self.notification_thread_stop_event.is_set():
-            self.notification_thread_stop_event.clear()
+        if not hasattr(self, 'notification_thread') or not self.notification_thread.is_alive():
+            self.notification_thread_stop_event = threading.Event()
+            self.notification_thread = threading.Thread(target=self.display_notifications, args=(5,), daemon=True)
+            self.notification_thread.start()
             self.toggle_button.config(text="Pause Notifications")
         else:
             self.notification_thread_stop_event.set()
+            self.notification_thread.join()
             self.toggle_button.config(text="Resume Notifications")
 
+    # add a word from word_list to noti_words_list
     def add_to_noti_words(self):
         selected = self.word_list.curselection()
         if selected:
@@ -86,6 +93,7 @@ class Platinum(tk.Tk):
             self.word_manager.add_to_noti_words(word)
             self.load_noti_words()
 
+    # remove a word from either list
     def delete_word(self):
         selected = self.word_list.curselection()
         if selected:
@@ -95,11 +103,13 @@ class Platinum(tk.Tk):
         selected = self.noti_words_list.curselection()
         if selected:
             word = self.noti_words_list.get(selected)
-            self.word_manager.delete_word(word)
+            self.word_manager.remove_from_noti_words(word)
             self.load_noti_words()
 
+    # system tray icon to exit the app
     def quit(self):
-        self.stop_notification_thread()
+        if hasattr(self, 'notification_thread'):
+            self.stop_notification_thread()
         if self.tray_icon:
             self.tray_icon.stop()
         try:
@@ -108,11 +118,13 @@ class Platinum(tk.Tk):
         except SystemExit:
             pass
 
+    # stop notifications function
     def stop_notification_thread(self):
-        if self.notification_thread.is_alive():
+        if hasattr(self, 'notification_thread') and self.notification_thread.is_alive():
             self.notification_thread_stop_event.set()
             self.notification_thread.join()
 
+    # on closing the app, minimizes it to system tray instead
     def hide_window(self):
         self.iconify()
         image = Image.open("icon.ico")
@@ -120,6 +132,7 @@ class Platinum(tk.Tk):
         self.tray_icon = pystray.Icon("platinum", image, "platinum", menu)
         self.tray_icon.run_detached()
 
+    # system tray icon function to reopen window
     def show_window(self):
         self.deiconify()
         if self.tray_icon:
--- word_manager.py
+++ word_manager.py
@@ -3,6 +3,7 @@ import json
 class WordManager:
     def __init__(self):
         self.words_file = "words.json"
+        self.noti_words_file = "noti_words.json"
         self.words = self.load_words()
         self.noti_words = self.load_noti_words()
 
@@ -18,42 +19,38 @@ class WordManager:
             json.dump(self.words, file)
 
     def add_word(self, word):
-        self.words.append(word)
-        self.save_words()
+        if word not in self.words:
+            self.words.append(word)
+            self.save_words()
 
     def delete_word(self, word):
         if word in self.words:
             self.words.remove(word)
+            self.save_words()
         if word in self.noti_words:
             self.noti_words.remove(word)
-        self.save_words()
+            self.save_noti_words()
 
     def get_words(self):
         return self.words
 
     def load_noti_words(self):
         try:
-            with open(self.words_file, "r") as file:
-                data = json.load(file)
-                if "noti_words" in data:
-                    return data["noti_words"]
-                else:
-                    return []
+            with open(self.noti_words_file, "r") as file:
+                return json.load(file)
         except FileNotFoundError:
             return []
 
     def save_noti_words(self):
-        try:
-            with open(self.words_file, "r") as file:
-                data = json.load(file)
-        except FileNotFoundError:
-            data = {}
-        data["noti_words"] = self.noti_words
-        with open(self.words_file, "w") as file:
-            json.dump(data, file)
+        with open(self.noti_words_file, "w") as file:
+            json.dump(self.noti_words, file)
 
     def add_to_noti_words(self, word):
         if word not in self.noti_words:
             self.noti_words.append(word)
-        self.save_noti_words()
-        return self.words
+            self.save_noti_words()
+
+    def remove_from_noti_words(self, word):
+        if word in self.noti_words:
+            self.noti_words.remove(word)
+            self.save_noti_words()
